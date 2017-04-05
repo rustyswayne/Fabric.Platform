c94eae483c1f722ad6d2e8ba12609eb32524e9ba
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-05 14:59:48 -0400

    Added middleware to set the UserPrincipal into the Owin context.

---
+++ Fabric.Platform.Auth/IdTokenMiddleware.cs
@@ -0,0 +1,29 @@
+﻿
+using System.IdentityModel.Tokens.Jwt;
+using LibOwin;
+using Microsoft.IdentityModel.Tokens;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+namespace Fabric.Platform.Auth
+{
+    public class IdTokenMiddleware
+    {
+        private const string IdTokenHeader = "fabric-end-user";
+        public static AppFunc Inject(AppFunc next)
+        {
+            return env =>
+            {
+                var ctx = new OwinContext(env);
+                if (ctx.Request.Headers.ContainsKey(IdTokenHeader))
+                {
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    SecurityToken token;
+                    var userPrincipal = tokenHandler.ValidateToken(ctx.Request.Headers[IdTokenHeader],
+                        new TokenValidationParameters(), out token);
+                    ctx.Set(IdTokenHeader, userPrincipal);
+                }
+                return next(env);
+            };
+        }
+    }
+}
