a29118dc1ca736a82ce03d47a42f1a0ebe779cac
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-05 14:19:48 -0400

    Added new library project to share authorization middleware. Added auth middleware to verify scopes present in the access token.

---
+++ Fabric.Platform.Auth/AuthorizationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using LibOwin;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+namespace Fabric.Platform.Auth
+{
+    public class AuthorizationMiddleware
+    {
+        public static AppFunc Inject(AppFunc next, string[] requiredScopes)
+        {
+            return env =>
+            {
+                var ctx = new OwinContext(env);
+                var principal = ctx.Request.User;
+                if (principal != null)
+                {
+                    if (requiredScopes.Any(requiredScope => principal.HasClaim("scope", requiredScope)))
+                    {
+                        return next(env);
+                    }
+                }
+                ctx.Response.StatusCode = 403;
+                return Task.FromResult(0);
+            };
+        }
+    }
+}
