c11be2732c3b73ffb29df393d86d672d02d5a0c7
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-06 14:38:20 -0400

    Added a LogFactory to the logging package. Added an ElasticSearchConfiguration class to the shared package.

---
+++ Fabric.Platform.Logging/LogFactory.cs
@@ -0,0 +1,23 @@
+﻿using Fabric.Platform.Shared.Configuration;
+using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.Elasticsearch;
+
+namespace Fabric.Platform.Logging
+{
+    public class LogFactory
+    {
+        public static ILogger CreateLogger(LoggingLevelSwitch levelSwitch, ElasticSearchSettings elasticSearchSettings,
+            string clientName)
+        {
+            var sinkOptions = new ElasticsearchSinkOptions(elasticSearchSettings.GetElasticSearchUri())
+            {
+                IndexFormat = "logstash-" + clientName + "-{0:yyyy.MM.dd}"
+            };
+            return new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(levelSwitch)
+                .Enrich.FromLogContext()
+                .WriteTo.Elasticsearch(sinkOptions).CreateLogger();
+        }
+    }
+}
