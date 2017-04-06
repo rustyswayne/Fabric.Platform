c6df2c60e1de6b6ab92a4e745381132c95ea7a69
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-06 13:37:13 -0400

    Added a shared library to centralize concepts.

---
+++ Fabric.Platform.Shared/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fabric.Platform.Shared
+{
+    public static class Constants
+    {
+        public static class FabricHeaders
+        {
+            public const string IdTokenHeader = "fabric-end-user";
+            public const string CorrelationTokenHeaderName = "correlation-token";
+            public const string AuthenticationHeaderPrefix = "Bearer";
+        }
+
+        public static class FabricLogContextProperties
+        {
+            public const string CorrelationTokenContextName = "CorrelationToken";
+        }
+    }
+}
