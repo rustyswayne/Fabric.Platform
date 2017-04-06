f8806174a4bc6d6659831983d373f6091f596667
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-06 10:35:34 -0400

    Added a new project to share httpclientfactory.

---
+++ Fabric.Platform.Http/IHttpClientFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabric.Platform.Http
+{
+    public interface IHttpClientFactory
+    {
+        Task<HttpClient> Create(Uri uri, string requestScope);
+        Task<HttpClient> CreateWithAccessToken(Uri uri, string accessToken);
+    }
+}
