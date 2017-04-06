1d220098eb3fd4779c9176d73a01bf8d045ca8ab
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-06 15:28:53 -0400

    Added an Identity server settings class to the Shared project.

---
+++ Fabric.Platform.Shared/Configuration/IdentityServerConfidentialClientSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fabric.Platform.Shared.Configuration
+{
+    public class IdentityServerConfidentialClientSettings
+    {
+        public string Authority { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string[] Scopes { get; set; }
+    }
+}
