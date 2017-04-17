511f4a09750b1ff0918e3bd7ccbe5eda1b1fe813
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-17 12:48:59 -0400

    Added a test project with a few xUnit tests.

---
+++ Fabric.Platform.UnitTests/Shared/ElasticSearchSettingsTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using Fabric.Platform.Shared.Configuration;
+using Fabric.Platform.Shared.Exceptions;
+using System.Collections.Generic;
+using System;
+
+namespace Fabric.Platform.UnitTests.Shared
+{
+    public class ElasticSearchSettingsTests
+    {
+        [Theory, MemberData(nameof(MalformedData))]
+        public void GetElasticSearchUri_InvalidSettings_Throws(ElasticSearchSettings elasticSearchSettings)
+        {
+            Assert.Throws<FabricConfigurationException>(() => elasticSearchSettings.GetElasticSearchUri());
+        }
+
+        [Theory, MemberData(nameof(ValidData))]
+        public void GetElasticSearchUri_ValidSettings_CreatesUriSuccessfully(ElasticSearchSettings settings, Uri expectedUri)
+        {
+            Assert.Equal(expectedUri, settings.GetElasticSearchUri());
+        }
+
+        public static IEnumerable<object[]> MalformedData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {new ElasticSearchSettings() },
+                    new object[] { new ElasticSearchSettings { Server = "localhost", Port = "9200" } },
+                    new object[] {new ElasticSearchSettings { Scheme = "http", Port = "9200" } },
+                    new object[] { new ElasticSearchSettings { Scheme = "localhost", Server = "localhost" } }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> ValidData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new ElasticSearchSettings { Scheme = "http", Server = "localhost", Port = "9200" }, new Uri("http://localhost:9200") },
+                    new object[] { new ElasticSearchSettings { Scheme = "http", Server = "localhost", Port = "9200", Username = "user", Password = "password" }, new Uri("http://username:password@localhost:9200") },
+                    new object[] { new ElasticSearchSettings { Scheme = "http", Server = "localhost", Port = "9200", Username = "user" }, new Uri("http://localhost:9200") },
+                    new object[] { new ElasticSearchSettings { Scheme = "http", Server = "localhost", Port = "9200", Username = "user", Password = "password" }, new Uri("http://localhost:9200") }
+                };
+            }
+        }
+    }
+
+
+
+}
