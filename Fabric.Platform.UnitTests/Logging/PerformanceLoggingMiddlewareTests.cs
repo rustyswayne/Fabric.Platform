6e317d340e50f1b9952ba4e0bdc794217ba3d84a
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-18 10:55:28 -0400

    Added tests for Performance and Request logging middleware

---
+++ Fabric.Platform.UnitTests/Logging/PerformanceLoggingMiddlewareTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+using Fabric.Platform.Logging;
+using LibOwin;
+using Moq;
+using Serilog;
+
+namespace Fabric.Platform.UnitTests.Logging
+{
+    public class PerformanceLoggingMiddlewareTests
+    {
+        private readonly AppFunc _noOp = env => Task.FromResult(0);
+        [Fact]
+        public void PerformanceLoggingMiddleware_Inject_RecordsPerfLogs()
+        {
+            //Arrange
+            var ctx = new OwinContext
+            {
+                Request =
+                {
+                    Scheme = LibOwin.Infrastructure.Constants.Https,
+                    Path = new PathString("/logtest"),
+                    Method = "GET"
+                }
+            };
+
+            var loggerMock = new Mock<ILogger>();
+            loggerMock.Setup(logger => logger.ForContext<PerformanceLoggingMiddleware>()).Returns(() => loggerMock.Object);
+            loggerMock.Setup(logger => logger.Information(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<object>(), It.IsAny<Int64>())).Verifiable();
+
+            //Act
+            var pipeline = PerformanceLoggingMiddleware.Inject(_noOp, loggerMock.Object);
+            pipeline(ctx.Environment);
+            
+            loggerMock.Verify();
+        }
+    }
+}
---
+++ Fabric.Platform.UnitTests/Logging/RequestLoggingMiddlewareTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+using Fabric.Platform.Logging;
+using LibOwin;
+using Moq;
+using Serilog;
+
+namespace Fabric.Platform.UnitTests.Logging
+{
+    public class RequestLoggingMiddlewareTests
+    {
+        private readonly AppFunc _noOp = env => Task.FromResult(0);
+        [Fact]
+        public void RequestLoggingMiddleware_Inject_CallsLogger()
+        {
+            //Arrange
+            var ctx = new OwinContext
+            {
+                Request =
+                {
+                    Scheme = LibOwin.Infrastructure.Constants.Https,
+                    Path = new PathString("/logtest"),
+                    Method = "GET"
+                }
+            };
+            //ctx.Request.Headers.Add("TestHeader", new[] {"TestHeaderValue"});
+
+            var loggerMock = new Mock<ILogger>();
+            loggerMock.Setup(logger => logger.ForContext<RequestLoggingMiddleware>()).Returns(() => loggerMock.Object);
+            loggerMock.Setup(logger => logger.Information(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<object>(), It.IsAny<object>())).Verifiable();
+            loggerMock.Setup(logger => logger.Information(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<object>())).Verifiable();
+
+            //Act
+            var pipeline = RequestLoggingMiddleware.Inject(_noOp, loggerMock.Object);
+            pipeline(ctx.Environment);
+            
+            //Assert
+            loggerMock.Verify();
+        }
+    }
+}
