fd1a656700c4ee2aa2f1be63ff533fd235afda9e
michael.vidal <michael.vidal@healthcatalyst.com>
2017-06-09 10:05:06 -0400

    Added the ability to optionally log to a file.

--- Fabric.Platform.Logging/LogFactory.cs
+++ Fabric.Platform.Logging/LogFactory.cs
@@ -1,4 +1,5 @@
-﻿using Fabric.Platform.Shared.Configuration;
+﻿using System;
+using Fabric.Platform.Shared.Configuration;
 using Serilog;
 using Serilog.Core;
 using Serilog.Sinks.Elasticsearch;
@@ -8,17 +9,24 @@ namespace Fabric.Platform.Logging
     public class LogFactory
     {
         public static ILogger CreateLogger(LoggingLevelSwitch levelSwitch, ElasticSearchSettings elasticSearchSettings,
-            string clientName)
+            string clientName, bool writeToFile = false)
         {
             var sinkOptions = new ElasticsearchSinkOptions(elasticSearchSettings.GetElasticSearchUri())
             {
                 IndexFormat = "logstash-" + clientName + "-{0:yyyy.MM.dd}"
             };
-            return new LoggerConfiguration()
+            var loggerConfiguration = new LoggerConfiguration()
                 .MinimumLevel.ControlledBy(levelSwitch)
                 .Enrich.FromLogContext()
                 .WriteTo.ColoredConsole()
-                .WriteTo.Elasticsearch(sinkOptions).CreateLogger();
+                .WriteTo.Elasticsearch(sinkOptions);
+
+            if (writeToFile)
+            {
+                loggerConfiguration.WriteTo.RollingFile("Logs\\log-{Date}.txt");
+            }
+
+            return loggerConfiguration.CreateLogger();
         }
     }
 }
