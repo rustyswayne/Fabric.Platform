f503895b9e30b69fc25c23af40745b1bcc8f3ca6
Brian Smith <brian.smith@healthcatalyst.com>
2018-04-04 11:00:26 -0400

    added HttpClientFactory back to project for backwards compatibility

---
+++ Fabric.Platform.Http/HttpClientFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Fabric.Platform.Shared;
+
+namespace Fabric.Platform.Http
+{
+    public class HttpClientFactory : IHttpClientFactory
+    {
+        private readonly string _correlationToken;
+        private readonly string _subject;
+        private readonly TokenClient _tokenClient;
+
+        public HttpClientFactory(string tokenUrl, string clientId, string secret, string correlationToken, string subject)
+        {
+            _correlationToken = correlationToken;
+            _subject = subject;
+            _tokenClient = new TokenClient(tokenUrl, clientId, secret);
+        }
+        public async Task<HttpClient> Create(Uri uri, string requestScope)
+        {
+            var response = await _tokenClient.RequestClientCredentialsAsync(requestScope).ConfigureAwait(false);
+            return CreateWithAccessToken(uri, response.AccessToken);
+        }
+
+        public HttpClient CreateWithAccessToken(Uri uri, string accessToken)
+        {
+            var client = new HttpClient { BaseAddress = uri };
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            client.DefaultRequestHeaders.Add(Constants.FabricHeaders.CorrelationTokenHeaderName, _correlationToken);
+            if (!string.IsNullOrEmpty(_subject))
+            {
+                client.DefaultRequestHeaders.Add(Constants.FabricHeaders.SubjectNameHeader, _subject);
+            }
+            return client;
+        }
+    }
+}
---
+++ Fabric.Platform.Http/IHttpClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Fabric.Platform.Http
+{
+    public interface IHttpClientFactory
+    {
+        Task<HttpClient> Create(Uri uri, string requestScope);
+        HttpClient CreateWithAccessToken(Uri uri, string accessToken);
+    }
+}
