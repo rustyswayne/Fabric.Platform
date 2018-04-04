161939ff7db27c5e33273c64b8eb31b571674d86
Brian Smith <brian.smith@healthcatalyst.com>
2018-04-04 10:30:04 -0400

    converted HttpClientFactory to HttpRequestMessageFactory

---
+++ Fabric.Platform.Http/HttpRequestMessageFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Fabric.Platform.Shared;
+
+namespace Fabric.Platform.Http
+{
+    public class HttpRequestMessageFactory : IHttpRequestMessageFactory
+    {
+        private readonly string _correlationToken;
+        private readonly string _subject;
+        private readonly TokenClient _tokenClient;
+
+        public HttpRequestMessageFactory(string tokenUrl, string clientId, string secret, string correlationToken, string subject)
+        {
+            _correlationToken = correlationToken;
+            _subject = subject;
+            _tokenClient = new TokenClient(tokenUrl, clientId, secret);
+        }
+        public async Task<HttpRequestMessage> Create(HttpMethod httpMethod, Uri uri, string requestScope)
+        {
+            var response = await _tokenClient.RequestClientCredentialsAsync(requestScope).ConfigureAwait(false);
+            return CreateWithAccessToken(httpMethod, uri, response.AccessToken);
+        }
+
+        public HttpRequestMessage CreateWithAccessToken(HttpMethod httpMethod, Uri uri, string accessToken)
+        {
+            var httpRequestMessage = new HttpRequestMessage(httpMethod, uri);
+            httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            httpRequestMessage.Headers.Add(Constants.FabricHeaders.CorrelationTokenHeaderName, _correlationToken);
+            if (!string.IsNullOrEmpty(_subject))
+            {
+                httpRequestMessage.Headers.Add(Constants.FabricHeaders.SubjectNameHeader, _subject);
+            }
+            return httpRequestMessage;
+        }
+    }
+}
---
+++ Fabric.Platform.Http/IHttpRequestMessageFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Fabric.Platform.Http
+{
+    public interface IHttpRequestMessageFactory
+    {
+        Task<HttpRequestMessage> Create(HttpMethod httpMethod, Uri uri, string requestScope);
+        HttpRequestMessage CreateWithAccessToken(HttpMethod httpMethod, Uri uri, string accessToken);
+    }
+}
